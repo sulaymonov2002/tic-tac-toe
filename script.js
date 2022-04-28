bc91ba631e0ab7b5cb95610e2af2dcd8e21481cd
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-04-29 01:25:16 +0500

    ends of game face

---
+++ script.js
@@ -0,0 +1,10 @@
+//selecting all reuired elements
+const selectBox = document.querySelector(".select-box");
+const selectXBtn = selectBox.querySelector(".playerX");
+const selectOBtn = selectBox.querySelector(".playerO");
+
+
+window.onload = () => {
+    
+}
+ 
